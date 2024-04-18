485c0abdbe823fc198c1a18c85de2a080b73242b
Tony Fenoaina <tfenoaina@gmail.com>
2024-04-18 13:55:45 +0300

    Creation d'une classe entité OperationBancaire

---
+++ src/main/java/mg/tfenoaina/tpbanquetonyfenoaina/entities/OperationBancaire.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.tfenoaina.tpbanquetonyfenoaina.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+/**
+ *
+ * @author info5
+ */
+@Entity
+public class OperationBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private LocalDateTime dateOperation;
+    private int montant;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OperationBancaire)) {
+            return false;
+        }
+        OperationBancaire other = (OperationBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mg.tfenoaina.tpbanquetonyfenoaina.entities.OperationBancaire[ id=" + id + " ]";
+    }
+
+    public OperationBancaire(String description, int montant) {
+        this.description = description;
+        this.montant = montant;
+        dateOperation = LocalDateTime.now();
+    }
+    
+    
+}
